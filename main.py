e8bfb7ae62512b0381b362d089640332091a1dc3
AndrewBazen <andrew.bazen@gmail.com>
2025-03-19 23:24:43 +0100

    add stats.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import count_words, count_characters
 
 def print_report(file, word_count, char_counts):
     print(f"--- Begin report of {file} ---")
@@ -7,19 +8,7 @@ def print_report(file, word_count, char_counts):
             print(f"The '{char}' character was found {count} times\n")
     print(f"--- End report ---")
 
-def count_characters(text):
-    char_counts = {}
-    for char in text:
-        char = char.lower()  # convert to lowercase
-        if char in char_counts:
-            char_counts[char] += 1
-        else:
-            char_counts[char] = 1
-    return char_counts
 
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     with open("books/frankenstein.txt") as f:
